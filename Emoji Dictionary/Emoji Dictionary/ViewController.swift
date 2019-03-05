a77f64d28d6501fe52a7ba89d68754df52405c9f
MichaelShep <48259615+MichaelShep@users.noreply.github.com>
2019-03-05 21:04:25 +0000

    Added Emojis to the TableView

---
+++ Emoji Dictionary/Emoji Dictionary/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Emoji Dictionary
+//
+//  Created by Michael Shepherd on 05/03/2019.
+//  Copyright © 2019 Shepherd Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableview: UITableView!
+    
+    var emojis = ["😂", "😍", "😁", "😜", "🤪"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableview.dataSource = self
+        tableview.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+
+}
+
